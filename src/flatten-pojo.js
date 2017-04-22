6b0113b58387ce83d0e74b9cd84a2c13c8163ea1
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-04-22 13:44:08 -0600

    serialize POJO -> flatten POJO

--- src/flatten-pojo.js
+++ src/flatten-pojo.js
@@ -3,14 +3,14 @@
 This file is for turning a POJO into a sequential array of the scalars it contains, and then
 reversing the transformation. This is done with two functions:
 
-serializePOJO(pojo)
+flattenPOJO(pojo)
   ASSUMES pojo is a POJO
   Outputs an array containing all the scalars in pojo arranged in a deterministic linear sequence.
 
-deserializePOJO(referencePojo, array)
+unflattenPOJO(referencePojo, array)
   ASSUMES
     pojo is a POJO
-    array is the result of calling serializePOJO on some POJO congruent to pojo
-  Outputs a POJO newPojo which is congruent to pojo, such that serializePOJO(newPojo) outputs array
+    array is the result of calling flattenPOJO on some POJO congruent to pojo
+  Outputs a POJO newPojo which is congruent to pojo, such that flattenPOJO(newPojo) outputs array
 
 */
