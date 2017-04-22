b6b7993de2e32ccbdd34fa2cbc9ff92f04a83aeb
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-04-22 14:42:00 -0600

    Add test for expected output of flattenPOJO.

--- test/flatten-pojo.js
+++ test/flatten-pojo.js
@@ -1,6 +1,14 @@
 import { expect } from 'chai';
 import { flattenPOJO, unflattenPOJO } from '../src/flatten-pojo.js';
 
+describe('flattenPOJO', function() {
+  it('should produce the expected output', function() {
+    for (let i = 0; i < testPojos.length; i++) {
+      expect(flattenPOJO(testPojos[i])).to.eql(testPojosFlattened[i]);
+    }
+  });
+});
+
 describe('flattenPOJO and unflattenPOJO', function() {
   it('should be inverses of each other', function() {
     testPojos.forEach(function(pojo) {
@@ -24,4 +32,19 @@ const testPojos = [
   { x: undefined },
   { x: { x: { } } },
   { y: [0, "foo", { y: true, a: [] }], b: undefined, a: [5.52, [1.0, [], 2.0], 0.004],  c: -0.0 },
+];
+
+const testPojosFlattened = [
+  [4],
+  [undefined],
+  [NaN],
+  [],
+  [],
+  [0],
+  [],
+  [],
+  [null],
+  [undefined],
+  [],
+  [5.52, 1.0, 2.0, 0.004, undefined, -0.0, 0, "foo", true],
 ];
