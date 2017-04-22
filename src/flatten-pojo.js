ce2943f2a2fc1c68b84d8676f0d80d62609dcb6f
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-04-22 14:36:13 -0600

    Implement flattenPOJO and unflattenPOJO.

--- src/flatten-pojo.js
+++ src/flatten-pojo.js
@@ -16,12 +16,56 @@ unflattenPOJO(referencePojo, array)
 
 */
 
-function flattenPOJO(pojo) {
+import {
+  isScalar
+} from './pojo.js';
 
+function flattenPOJO(pojo) {
+  let results = [];
+  flattenPOJOtraverse(pojo, results);
+  return results;
+}
+
+
+function flattenPOJOtraverse(pojo, results) {
+  if (isScalar(pojo)) {
+    results.push(pojo);
+  } else if (pojo instanceof Array) {
+    pojo.forEach((subpojo) => flattenPOJOtraverse(subpojo, results));
+  } else {
+    // pojo is a plain object, depending on the assumption that it is a POJO
+    let keys = Object.keys(pojo).sort();
+    keys.forEach((key) => flattenPOJOtraverse(pojo[key], results));
+  }
 }
 
 function unflattenPOJO(referencePojo, array) {
+  return unflattenPOJOrecurse(referencePojo, array, 0)[0];
+}
 
+function unflattenPOJOrecurse(referencePojo, array, index) {
+  if (isScalar(referencePojo)) {
+    return [array[index], index+1];
+  } else if (referencePojo instanceof Array) {
+    var result = [];
+    for (let i = 0; i < referencePojo.length; i++) {
+      let [subResult, nextIndex] = unflattenPOJOrecurse(referencePojo[i], array, index);
+      index = nextIndex;
+      result.push(subResult);
+    }
+    return [result, index];
+  } else {
+    // referencePojo is a plain object, depending on the assumption that it is a POJO
+    var result = {};
+    let keys = Object.keys(referencePojo).sort();
+    for (let i = 0; i < keys.length; i++) {
+      let key = keys[i];
+      let [keyValue, nextIndex] = unflattenPOJOrecurse(referencePojo[key], array, index);
+      index = nextIndex;
+      result[key] = keyValue;
+    }
+    return [result, index];
+  }
 }
 
 export { flattenPOJO, unflattenPOJO };
