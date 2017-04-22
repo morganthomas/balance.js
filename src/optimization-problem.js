8ec305cf1cb8cbc28c9e0f1748c8eca2dfc013cf
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-04-22 16:01:29 -0600

    Add non-working implementation of solveOptimizationProblem.

--- src/optimization-problem.js
+++ src/optimization-problem.js
@@ -13,15 +13,35 @@ where:
 
  * objectiveFunction is a differentiable scalar field.
  * initialGuessFunction is a function.
-    * Assumes that as input it gets one PONuNJO congruent to objective.inputClassRepr.
-    * As output it produces a PONJO congruent to objective.inputClassRepr, where every
-      number equals the number in the same location in the input if the same location in the
-      input does not have value null.
+ * Assumes that as input it gets one PONuNJO congruent to objective.inputClassRepr.
+ * As output it produces a PONJO congruent to objective.inputClassRepr, where every
+   number equals the number in the same location in the input if the same location in the
+   input does not have value null.
 
 */
 
+import { uncmin } from 'numeric';
+import { flattenPOJO, unflattenPOJO } from './flatten-pojo.js';
+
 function solveOptimizationProblem(optimizationProblem, constraints) {
+  let initialGuess = optimizationProblem.initialGuessFunction(constraints);
+  let initialGuessFlat = flattenPOJO(initialGuess);
+  let { valueAt, gradientAt, inputClassRepr } = optimizationProblem.objectiveFunction;
   
+  let valueAtFlat = (x) =>
+      valueAt(unflattenPOJO(inputClassRepr, x));
+
+  let gradientAtFlat = (x) =>
+      flattenPOJO(gradientAt(unflattenPOJO(inputClassRepr, x)));
+
+  let solutionFlat = uncmin(
+    optimizationProblem.objectiveFunction.valueAt,
+    initialGuessFlat,
+    valueAtFlat,
+    gradientAtFlat
+  );
+
+  return unflattenPOJO(inputClassRepr, solutionFlat);
 }
 
 export { solveOptimizationProblem };
