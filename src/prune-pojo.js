eb9447be3433c56717c026063092b87eddea4000
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-04-22 18:42:07 -0600

    add non-working definition of prunePOJO

--- src/prune-pojo.js
+++ src/prune-pojo.js
@@ -1,5 +1,56 @@
-function prunePOJO(predicate, pojo) {
+import { isPOJO, isScalar } from './pojo.js';
 
+// This function is for removing stuff you don't need from a POJO. It takes a predicate, and
+// tests the predicate against every scalar value in the POJO. It throws out the locations in the
+// POJO with a scalar value for which the predicate returns falsy. It also throws out empty arrays
+// and empty objects. Generally, it throws away everything you don't need, assuming that 'predicate'
+// tests a scalar value to say whether you need that scalar value.
+function prunePOJO(predicate, pojo) {
+  assert(isPOJO(pojo));
+  if (isScalar(pojo)) {
+    return pojo;
+  } else if (pojo instanceof Array) {
+    let result = [];
+    for (let i = 0; i < pojo.length; i++) {
+      let subPojo = prunePOJO(predicate, pojo[i]);
+      if (isScalar(subPojo)) {
+        if (predicate(subPojo)) {
+          result.push(subPojo);
+        }
+      } else if (subPojo instanceof Array) {
+        if (subPojo.length > 0) {
+          result.push(subPojo);
+        }
+      } else {
+        // subPojo is a plain object, depending on the assumption that it is a POJO
+        if (Object.keys(subPojo).length > 0) {
+          result.push(subPojo);
+        }
+      }
+    }
+    return result;
+  } else {
+    // pojo is a plain object, depending on the assumption that it is a POJO
+    let result = {};
+    for (let key in pojo) {
+      let subPojo = prunePOJO(predicate, pojo[key]);
+      if (isScalar(subPojo)) {
+        if (predicate(subPojo)) {
+          result[key] = subPojo;
+        }
+      } else if (subPojo instanceof Array) {
+        if (subPojo.length > 0) {
+          result[key] = subPojo;
+        }
+      } else {
+        // subPojo is a plain object, depending on the assumption that it is a POJO
+        if (Object.keys(subPojo.length > 0)) {
+          result[key] = subPojo;
+        }
+      }
+    }
+    return result;
+  }
 }
 
 function coprunePOJO(predicate, originalPojo, prunedPojo) {
