a9acd759a4e6a6240a7ecfae9c58e21e3a8d3ddc
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-04-23 09:46:38 -0600

    update constrainOptimizationProblem docs

--- src/constrain-optimization-problem.js
+++ src/constrain-optimization-problem.js
@@ -13,4 +13,29 @@ An equivalence class, by definition, is an array equiv such that:
 
 No path may occur in two equivalence classes in the constraints array.
 
+The return value is a new optimization problem: call it constrainedProblem. This return value has,
+in addition to the properties objectiveFunction and initialGuessFunction required by the definition of
+optimization problems, two additional properties, both functions:
+
+constrainedProblem.constrainPONJO(ponjo)
+ * Takes as input a PONJO, 'ponjo', which must be structurally congruent to
+   optimizationProblem.objectiveFunction.domainRepresentative.
+ * Produces as output a PONJO which is structurally congruent to
+   constrainedProblem.objectiveFunction.domainRepresentative.
+ * This function converts between the domains of the two optimization problems, by removing
+   fields which are rendered redundant by the constraints.
+
+constrainedProblem.unconstrainPONJO(ponjo)
+ * Takes as input a PONJO, 'ponjo', which must be structurally congruent to
+   constrainedProblem.objectiveFunction.domainRepresentative.
+ * Produces as output a PONJO which is structurally congruent to
+   optimizationProblem.objectiveFunction.domainRepresentative.
+ * This function converts between the domains of the two optimization problems, by adding back in
+   fields which are rendered redundant by the constraints.
+
+unconstrainPONJO is a right inverse of constrainPONJO, meaning that for any PONJO 'ponjo' which is
+structurally congruent to constrainedProblem.objectiveFunction.domainRepresentative,
+constrainedProblem.constrainPONJO(constrainedProblem.unconstrainPONJO(ponjo)) is
+deep equal to ponjo.
+
 */
