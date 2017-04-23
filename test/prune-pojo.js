78fbb55b5e2ec3b635cf77c89725f870d69a0f94
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-04-22 18:28:15 -0600

    Add prunePOJO test case.

--- test/prune-pojo.js
+++ test/prune-pojo.js
@@ -3,7 +3,20 @@ import { prunePOJO, coprunePOJO } from '../src/prune-pojo.js';
 
 describe('prunePOJO', function() {
   it('gives the expected results', function() {
-
+    expect(prunePOJO(x => !!x, {
+      a: null,
+      b: false,
+      c: undefined,
+      d: NaN,
+      e: 'foo',
+      f: '',
+      g: [],
+      h: [0.0, 1.0, 'bar', { a: null, e: 'h', f: { } }],
+      i: { x: { x: { y: [], z: { } } } }
+    })).to.eql({
+      e: 'foo',
+      h: [1.0, 'bar', { e: 'h' }],
+    });
   });
 });
 
