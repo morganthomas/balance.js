53e00ec9c20f22b03a95798bfb2353618ac7e78b
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-04-23 09:35:01 -0600

    start documenting constrainOptimizationProblem

---
+++ src/constrain-optimization-problem.js
@@ -0,0 +1,14 @@
+/*
+
+This file is for applying equality constraints to optimization problems, in order to produce new
+optimization problems. It defines the following functions:
+
+== constrainOptimizationProblem(optimizationProblem, constraints) ==
+
+'optimizationProblem' is an optimization problem. 'constraints' is a list of equivalence classes. 
+An equivalence class, by definition, is an array equiv such that:
+ * at most one element of equiv is either a number or a function; and,
+ * all other elements of equiv are paths (i.e. arrays of positive integers and/or strings);
+   said paths must exist in optimizationProblem.objectiveFunction.domainRepresentative.
+
+*/
