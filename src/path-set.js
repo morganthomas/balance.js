795b7b68b9765e6ae96e23ca1991dcb8552a32e6
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-04-23 20:44:27 -0600

    implement arrayToPathSet

--- src/path-set.js
+++ src/path-set.js
@@ -1,9 +1,10 @@
 /*
 
-Path sets are an efficient way of representing finite sets of paths (see path.js).
+Path sets are an efficient way of representing finite sets of non-empty paths (see path.js).
+A non-empty path is in other words a path with at least one element.
 
 arrayToPathSet(paths)
-  Takes an array of paths and returns the set of those paths.
+  Takes an array of non-empty paths and returns the set of those paths.
 
 pathSetContains(set, path);
   Returns true if the given path set contains the given path.
@@ -13,8 +14,44 @@ pathSetToArray(set)
 
 */
 
-function arrayToPathSet(paths) {
+import assert from 'assert';
+import { isPath } from './path.js';
 
+function arrayToPathSet(paths) {
+  assert(paths instanceof Array);
+  assert(paths.every(path => isPath(path) && path.length > 0));
+
+  let result = {};
+
+  for (let i = 0; i < paths.length; i++) {
+    let path = paths[i];
+    let position = result;
+    for (let j = 0; j < path.length; j++) {
+      let key = path[j];
+      if (j + 1 === path.length) {
+        // this is the last key in the path
+        if (!position.hasOwnProperty(key)) {
+          position[key] = true;
+        } else if (position[key] instanceof Array) {
+          position[key][0] = true;
+        } else {
+          // in this case position[key] === true and we don't need to do anything
+        }
+      } else {
+        // this is not the last key in the path
+        if (!position.hasOwnProperty(key) || position[key] === true) {
+          let newPosition = {};
+          position[key] = [position[key] || false, newPosition];
+          position = newPosition;
+        } else {
+          // in this case: position[key] instanceof Array
+          position = position[key][1];
+        }
+      }
+    }
+  }
+
+  return result;
 }
 
 export {
