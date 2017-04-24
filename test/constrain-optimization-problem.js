13770c3830b4e21a2721be32d8ceae3f355fa58c
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-04-23 21:53:48 -0600

    refactor test file

--- test/constrain-optimization-problem.js
+++ test/constrain-optimization-problem.js
@@ -21,45 +21,45 @@ describe('getPathsToPrune', function() {
 });
 
 describe('constrainOptimizationProblem', function() {
-  it('gives the expected output on a simple case', function() {
-    let objectiveFunction = {
-      domainRepresentative: {
-        x: 0,
-        y: 0,
-        z: [0, 0]
-      },
-      valueAt: (v) => v.x + v.y + v.z[0] * v.z[1],
-      gradientAt: (v) => ({
-        x: 1,
-        y: 1,
-        z: [v.z[1], v.z[0]]
-      })
-    };
+  let objectiveFunction = {
+    domainRepresentative: {
+      x: 0,
+      y: 0,
+      z: [0, 0]
+    },
+    valueAt: (v) => v.x + v.y + v.z[0] * v.z[1],
+    gradientAt: (v) => ({
+      x: 1,
+      y: 1,
+      z: [v.z[1], v.z[0]]
+    })
+  };
 
-    let initialGuessFunction = function(constraints) {
-      return mapScalars(x => x === null ? 0.0 : x, constraints);
-    };
+  let initialGuessFunction = function(constraints) {
+    return mapScalars(x => x === null ? 0.0 : x, constraints);
+  };
 
-    let problem = {
-      objectiveFunction,
-      initialGuessFunction
-    };
+  let problem = {
+    objectiveFunction,
+    initialGuessFunction
+  };
 
-    let constraintsList = [
-      [],
-      [[['x'], ['y']]],
-      [[['x'], ['y']], [['z', 0], ['z', 1]]],
-    ];
+  let constraintsList = [
+    [],
+    [[['x'], ['y']]],
+    [[['x'], ['y']], [['z', 0], ['z', 1]]],
+  ];
 
-    let constrainedProblems = constraintsList.map(
-      constraints => constrainOptimizationProblem(problem, constraints));
+  let constrainedProblems = constraintsList.map(
+    constraints => constrainOptimizationProblem(problem, constraints));
 
-    let expectedRepresentatives = [
-      { x: 0, y: 0, z: [0, 0] },
-      { x: 0, z: [0, 0] },
-      { x: 0, z: [0] }
-    ];
-
+  let expectedRepresentatives = [
+    { x: 0, y: 0, z: [0, 0] },
+    { x: 0, z: [0, 0] },
+    { x: 0, z: [0] }
+  ];
+  
+  it('gives the expected domain representatives', function() {
     constrainedProblems.forEach(
       (problem, i) => expect(problem.objectiveFunction.domainRepresentative)
         .to.eql(expectedRepresentatives[i]));
