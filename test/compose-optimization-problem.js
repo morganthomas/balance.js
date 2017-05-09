1ace6c4a181151eb9eeed245e2e3e8e52adce67f
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-05-08 22:19:35 -0600

    stub out composeOptimizationProblem tests.

---
+++ test/compose-optimization-problem.js
@@ -0,0 +1,3 @@
+describe('composeOptimizationProblem', function() {
+
+});
--- test/test.js
+++ test/test.js
@@ -5,3 +5,4 @@ import './flatten-pojo.js';
 import './prune-pojo.js';
 import './optimization-problem.js';
 import './constrain-optimization-problem.js';
+import './compose-optimization-problem.js';
