33eaac0b24669f14bee30900a336772629a5e677
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-05-13 09:15:21 -0600

    update composeDifferentiableScalarFields comments.

--- src/compose-differentiable-scalar-fields.js
+++ src/compose-differentiable-scalar-fields.js
@@ -7,10 +7,20 @@ differentiable scalar field (see scalar-field.js).
 
 options is an object of the following form:
   {
-    domain: ...,
+    domainRepresentative: ...,
     subfields: [ ... ],
     subdomainMaps: [ ... ],
     integrator: ...,
   }
+
+ * domainRepresentative is a PONJO which will represent the domain of the produced scalar field.
+ * subfields is an array of differentiable scalar fields. 
+ * subdomainMaps is an array of functions of the same length as the subfields
+   array. subdomainMaps[i] should map any PONJO congruent to domainRepresentative to a PONJO congruent
+   to subfields[i].domainRepresentative.
+ * integrator is a differentiable scalar field whose input domain should be represented by
+   [domainRepresentative, [1,...,subfields.length]] (in other words a pair consisting of
+   a first element congruent to domainRepresentative and a second element which is an array of
+   scalars of the same length as the subfields array).
 
 */
