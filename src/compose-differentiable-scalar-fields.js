112fbc5c6337bde5b386f42c3af821f232242ac1
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-05-13 09:05:34 -0600

    add compose-differentiable-scalar-fields.js

---
+++ src/compose-differentiable-scalar-fields.js
@@ -0,0 +1,16 @@
+/*
+
+This file is for composing multiple differentiable scalar fields to produce a new
+differentiable scalar field (see scalar-field.js).
+
+== composeDifferentiableScalarFields(options) ==
+
+options is an object of the following form:
+  {
+    domain: ...,
+    subfields: [ ... ],
+    subdomainMaps: [ ... ],
+    integrator: ...,
+  }
+
+*/
