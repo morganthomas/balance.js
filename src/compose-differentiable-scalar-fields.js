92840ffec13013a47c66aaecba9f514ecb05f78e
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-05-13 20:01:53 -0600

    update composeDifferentiableScalarFields comments.

--- src/compose-differentiable-scalar-fields.js
+++ src/compose-differentiable-scalar-fields.js
@@ -9,27 +9,24 @@ options is an object of the following form:
   {
     domainRepresentative: ...,
     subfields: [ ... ],
-    subdomainMaps: [ ... ],
-    integrator: ...,
+    valueAt(x, subfieldValues),
+    gradientAt(x, subfieldValues, subfieldGradients),
   }
 
  * domainRepresentative is a PONJO which will represent the domain of the produced scalar field.
- * subfields is an array of differentiable scalar fields. 
- * subdomainMaps is an array of functions of the same length as the subfields
-   array. subdomainMaps[i] should map any PONJO congruent to domainRepresentative to a PONJO congruent
-   to subfields[i].domainRepresentative.
- * integrator is a differentiable scalar field whose input domain should be represented by
-   [domainRepresentative, [1,...,subfields.length]] (in other words represented by a pair consisting
-   of a first element congruent to domainRepresentative and a second element which is an array of
-   scalars of the same length as the subfields array).
+ * subfields is an array of differentiable scalar fields with subfields[i].domainRepresentative
+   congruent to domainRepresentative for all i.
+ * valueAt and gradientAt are functions.
 
 The resulting composed differentiable scalar field can be described by the formula:
 
-  f(x) =
-    integrator(x, [
-      subfields[0](subdomainMaps[0](x)),
-      ...,
-      subfields[subfields.length-1](subdomainMaps[subfields.length-1](x))
-    ])
+  f.valueAt(x) = options.valueAt(x,
+    [subfields[0].valueAt(x), ..., subfields[subfields.length-1].valueAt(x)])
+
+And its gradient can be described by the formula:
+
+  f.gradientAt(x) = options.gradientAt(x,
+    [subfields[0].valueAt(x), ..., subfields[subfields.length-1](x)],
+    [subfields[0].gradientAt(x), ..., subfields[subfields.length-1].gradientAt(x)])
 
 */
