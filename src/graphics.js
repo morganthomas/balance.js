ddd67dff497fc0b49572e5c730b9a494e13e6ed9
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-05-13 23:48:18 -0600

    update graphics comments

--- src/graphics.js
+++ src/graphics.js
@@ -1,6 +1,21 @@
 /*
 
-This file describes a device-independent vector graphics representation.
+This file describes a medium-independent, POJO-based vector graphics representation.
+Graphics objects can take the following forms:
+
+ * { line: { from: { x, y }, to: { x, y }, color: { r, g, b } } }
+ * { translate: { what, by: { x, y } } }
+ * Any array of graphics objects is a graphics object.
+
+In these forms:
+
+ * x and y are numbers in arbitrary coordinate space units, which should be CSS pixels
+   in the initial coordinate space but could be modified by transformations.
+   In the initial coordinate space, { x: 0, y: 0 } should be the bottom left corner
+   of the viewport; increasing x should move towards the right; and increasing y should move
+   towards the top.
+ * r, g, b are numbers between 0 and 1.
+ * 'what' is a graphics object.
 
 TODO
 
