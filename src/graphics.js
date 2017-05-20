34afca3ce8013783e42713ca7e7b49e65857742b
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-05-19 22:00:16 -0600

    get very simple graphics drawing working.

--- src/graphics.js
+++ src/graphics.js
@@ -3,19 +3,25 @@
 This file describes a medium-independent, POJO-based vector graphics representation.
 Graphics objects can take the following forms:
 
- * { line: { from: { x, y }, to: { x, y }, color: { r, g, b } } }
+ * { stroke: { start: { x, y }, motions, color: { r, g, b }, width, lineCap } }
  * { translate: { what, by: { x, y } } }
  * Any array of graphics objects is a graphics object.
 
 In these forms:
 
- * x and y are numbers in arbitrary coordinate space units, which should be CSS pixels
+ * x, y, width are numbers in arbitrary coordinate space units, which should be CSS pixels
    in the initial coordinate space but could be modified by transformations.
-   In the initial coordinate space, { x: 0, y: 0 } should be the bottom left corner
+   In the initial coordinate space, { x: 0, y: 0 } should be the top left corner
    of the viewport; increasing x should move towards the right; and increasing y should move
-   towards the top.
+   towards the bottom.
+ * lineCap is one of 'butt', 'round', or 'square'.
  * r, g, b are numbers between 0 and 1.
  * 'what' is a graphics object.
+ * 'motions' is an array of motion objects.
+
+Motion objects can take the following forms:
+
+ * { lineTo: { x, y } }
 
 == drawToCanvas(canvas, graphics) ==
 
@@ -23,3 +29,21 @@ Takes an HTML canvas element and a graphics object, and draws the given graphics
 to the given canvas.
 
 */
+
+function drawToCanvas(canvas, graphics) {
+  var ctx = canvas.getContext('2d');
+  if (graphics.stroke) {
+    let stroke = graphics.stroke;
+    ctx.lineWidth = stroke.width;
+    ctx.strokeStyle = 'rgb(' + stroke.color.r + ', ' + stroke.color.g + ', ' + stroke.color.b + ', '+ stroke.color.a + ')';
+    ctx.lineCap = stroke.lineCap;
+    ctx.beginPath();
+    ctx.moveTo(stroke.start.x, stroke.start.y);
+    stroke.motions.forEach(function(motion) {
+      if (motion.lineTo) {
+        ctx.lineTo(motion.lineTo.x, motion.lineTo.y);
+      }
+    });
+    ctx.stroke();
+  }
+}
