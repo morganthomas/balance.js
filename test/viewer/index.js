9da8666111ab5d788a85b470ca614d7b8ff94121
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-05-28 16:01:56 -0600

    pass width and height constraints to test cases.

--- test/viewer/index.js
+++ test/viewer/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import line from './cases/line.js';
 import line2 from './cases/line2.js';
+import { mapScalars } from '../../src/pojo.js';
 import { solveOptimizationProblem } from '../../src/optimization-problem.js';
 import { drawToCanvas } from '../../src/graphics.js';
 
@@ -24,7 +25,10 @@ const app = new Vue({
     drawCase() {
       let theCase = this.cases[this.selectedCaseName];
       let problem = theCase.layoutProblem;
-      let solution = solveOptimizationProblem(problem);
+      let constraints = mapScalars(() => null, problem.objectiveFunction.domainRepresentative);
+      constraints.width = this.$refs.canvas.width;
+      constraints.height = this.$refs.canvas.height;
+      let solution = solveOptimizationProblem(problem, constraints);
       let graphics = theCase.render(solution);
       drawToCanvas(this.$refs.canvas, graphics);
     },
