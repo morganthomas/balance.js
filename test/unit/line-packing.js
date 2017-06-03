19a53be698e60cda0504cacda69896b22a1c8332
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-06-03 14:57:21 -0600

    further rearrange line packing test boxes.

--- test/unit/line-packing.js
+++ test/unit/line-packing.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import { makeTestBox } from '../lib/test-box.js';
+import { testBoxes } from '../lib/line-packing.js';
 import { BLACK } from '../lib/colors.js';
 import { makeSoftConstraintField } from '../../src/scalar-fields/soft-constraint-field.js';
 
