ff792dc4cb9bcfccb4246ab24cae1e993b40da7e
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-06-02 20:27:46 -0600

    update line length comments

--- src/line-packing-2.js
+++ src/line-packing-2.js
@@ -2,7 +2,8 @@
 
 This module solves the problem of "line packing." This is the problem of fitting a finite number
 of "boxes" into a finite sequence of lines whose lengths are drawn from a given infinite sequence
-of lengths.
+of lengths. Prototypical applications are breaking words into lines to form paragraphs, and
+breaking lines up across pages.
 
 To specify a line packing problem requires two things: an array of "boxes," and a "line length
 function."
@@ -18,6 +19,16 @@ DEFINITION. A "box" is an object of the following form:
     [postBreakBox]
   }
 
-
+ * velement is a velement.
+ * lengthParameter is a path into velement.layoutProblem.objectiveFunction.domainRepresentative.
+   E.g. when line breaking English to make a paragraph, lengthParameter is ['width'].
+   When page breaking, lengthParameter is ['height'].
+ * optimalLength is a number, presumed to be an optimal setting for lengthParameter.
+ * isRigid is a boolean, representing whether this velement needs to be its optimalLength
+   or whether its length can vary.
+ * isBreakpoint is a boolean, representing whether this velement can be replaced with a line
+   break preceded by preBreakBox and followed by postBreakBox.
+ * preBreakBox and postBreakBox are optional parameters which are only meaningful if isBreakpoint
+   is true.
 
 */
