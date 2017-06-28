4371a71d0bfe75219805d8002e2634a59c4215e0
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2017-06-27 22:54:20 -0600

    add comments on non-negative constraint field.

---
+++ src/scalar-fields/non-negative-constraint.js
@@ -0,0 +1,8 @@
+/*
+
+This file lets you make scalar fields which impose a soft constraint that a specified
+parameter must be non-negative.
+
+== makeNonNegativeConstraintField(domainRepresentative, parameter) ==
+
+*/
